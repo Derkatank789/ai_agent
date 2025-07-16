9cef1329e379ca42d9ff78a7f47b2bbf0c843152
derkatank789 <derkatank789@gmail.com>
2025-07-16 03:37:55 -0500

    Updated to Accept input

--- main.py
+++ main.py
@@ -8,13 +8,19 @@ from google import genai
 
 client = genai.Client(api_key=api_key)
 
+import sys
+
 def main():
+    sys_input = sys.argv
     print("Hello from ai-agent!")
-    respones = client.models.generate_content(model = "gemini-2.0-flash-001", 
-                                              contents = "Why is Boot.dev such a great place to learn backend development?" \
-                                              " Use one paragraph maximum.")
-    print(respones.text)
-    print(f"Prompt tokens: {respones.usage_metadata.prompt_token_count}")
-    print(f"Response tokens: {respones.usage_metadata.candidates_token_count}")
+    if len(sys_input) <= 1:
+        sys.exit(1)
+    else:
+        respones = client.models.generate_content(model = "gemini-2.0-flash-001", 
+                                              contents = sys_input[1])
+        print(respones.text)
+        print(f"Prompt tokens: {respones.usage_metadata.prompt_token_count}")
+        print(f"Response tokens: {respones.usage_metadata.candidates_token_count}")
+    
 if __name__ == "__main__":
     main()
